3a8d879f7e68c3cc258b7bb623437a01312f36e6
Pedro Henrique (Styxx) <pedrostyxx@protonmail.com>
2022-08-05 23:02:39 -0300

    Criptografia Adcionada
    A conexão só funciona se os dois digitarem a mesma key no terminal

--- client.py
+++ client.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import cryptocode
 import socket
 import os
 os.system('clear')
 
 
 def ClientMain():
-    server_ip = str(input('CONNECTION IP (Ex: "localhost"): '))  # Ip que será usado na conexão
-
-    server_port = int(input('CONNECTION PORT (Ex: "8888"): '))  # Porta que será usada na conexão
+    # Ip que será usado na conexão
+    server_ip = str(input('CONNECTION IP (Ex: "localhost"): '))
+    server_port = int(input('CONNECTION PORT (Ex: "8888"): ')
+                      )  # Porta que será usada na conexão
+    key = input('Key: ')
 
     os.system('clear')
 
     color_yellow = '\033[1;33m'
     color_reset = '\033[0;0m'
+    color_cyan = '\033[1;36m'
 
     # Comunicação de client-side e server-side através do protocolo TCP-IP
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     # Estabelece conexão com ip e porta informados
     client.connect((server_ip, server_port))
     print('Connected!')
@@ -28,14 +33,15 @@ def ClientMain():
     while not end:
         # Informação de quantidade de bits passando e decodificação utf-8
         # Mensagem que será enviada
-        client.send(input('Message: ').encode('utf-8'))
+        client.send(cryptocode.encrypt(input(f'{color_cyan}(YOU) -->{color_reset}  '), key).encode('utf-8'))
         msg = client.recv(1024).decode('utf-8')
 
         if msg == '/exit':  # Mensagem para fim de conexão
             end = True
         else:
+            # Print da msg recebida
             print(
-                f'{color_yellow}(SERVER)({server_ip}:{server_port}){color_reset} --> ' + msg)  # Print da msg recebida
+                f'{color_yellow}(SERVER)({server_ip}:{server_port}){color_reset} --> ' + cryptocode.decrypt(msg, key))
     client.close()  # Fecha a conexão com client
     exit()  # Fecha script
 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import cryptocode
 import socket
 import os
 os.system('clear')
@@ -7,13 +8,14 @@ os.system('clear')
 
 def ServerMain():
     server_ip = str(input('CONNECTION IP (Ex: "localhost"): '))  # Ip que será usado na conexão
-
     server_port = int(input('CONNECTION PORT (Ex: "8888"): '))  # Porta que será usada na conexão
-    
+    key = input('Key: ')
+
     os.system('clear')
 
     color_yellow = '\033[1;33m'
     color_reset = '\033[0;0m'
+    color_cyan = '\033[1;36m'
 
     # Comunicação de client-side e server-side através do protocolo TCP-IP
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,9 +39,9 @@ def ServerMain():
             end = True
         else:
             print(
-                f'{color_yellow}(CLIENT)({server_ip}:{server_port}){color_reset} --> ' + msg)  # Print de msg recebida
+                f'{color_yellow}(CLIENT)({server_ip}:{server_port}){color_reset} --> ' + cryptocode.decrypt(msg, key))  # Print de msg recebida
         # Mensagem que será enviada
-        client.send(input('Message: ').encode('utf-8'))
+        client.send(cryptocode.encrypt(input(f'{color_cyan}(YOU) -->{color_reset}  '), key).encode('utf-8'))
     client.close()  # Fecha conexão com client
     server.close()  # Fecha conexão com server
     exit()  # Fecha script
